fc5c767c229a2a39efe44fcabbf32893bd9561c1
Theo Biebele Daminabo <dammybeo@gmail.com>
2024-10-30 09:49:49 -0700

    the last edit on mobile menu

--- main.js
+++ main.js
@@ -6,6 +6,14 @@ document.querySelector('.toggle__menu').addEventListener('click',()=>{
     const toggle = document.querySelector('.toggle__menu')
     const menu = document.querySelector('nav ul')
 
+    document.body.style.backgroundColor='var(--darkbg)'
     menu.classList.toggle('menu-active')
     toggle.classList.toggle('active')
+    if(toggle.classList.contains('active')){
+        return  document.querySelector('main').style.display='none'
+        
+    }else{
+        return  document.querySelector('main').style.display='block'
+        
+    }
 })
